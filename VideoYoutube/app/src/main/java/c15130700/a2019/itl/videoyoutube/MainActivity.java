09adc9644bf192a43aa2ffcc616281b864eccf2d
Liliana Gallegos <lily.gallegos@hotmail.com>
2019-05-22 21:52:52 -0500

    Dos proyectos
    agregue los proyectos VideoYT y NavActivity, falta documentación

---
+++ VideoYoutube/app/src/main/java/c15130700/a2019/itl/videoyoutube/MainActivity.java
@@ -0,0 +1,29 @@
+package c15130700.a2019.itl.videoyoutube;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import java.util.Vector;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    Vector<YouTubeVideos> youtubeVideos = new Vector<YouTubeVideos>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager( new LinearLayoutManager(this));
+
+        youtubeVideos.add( new YouTubeVideos("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/MV4-oGJ6ZNQ\" frameborder=\"0\" allowfullscreen></iframe>") );
+        youtubeVideos.add( new YouTubeVideos("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/uhQ7mh_o_cM\" frameborder=\"0\" allowfullscreen></iframe>") );
+
+        VideoAdapter videoAdapter = new VideoAdapter(youtubeVideos);
+
+        recyclerView.setAdapter(videoAdapter);
+    }
+}
